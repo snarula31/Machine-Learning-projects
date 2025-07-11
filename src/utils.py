db8846616bcbdf6308e8f86ed81237295aedf77e
Shivansh <shivanshnarula@gmail.com>
2025-07-11 19:17:26 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -18,3 +18,22 @@ def save_object(file_path, obj):
         logging.info(f"Object saved at {file_path}")
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    This function evaluates the given models and returns a report of their performance.
+    """
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            models = list(models.values())[i]
+            models.fit(X_train, y_train)
+            y_train_pred = models.predict(X_train)
+            y_test_pred = models.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys) from e
